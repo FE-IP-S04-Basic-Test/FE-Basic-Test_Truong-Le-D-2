494894b4462a29aa49e8909eae784501ba52c70f
letruong <truongld@runsystem.net>
2023-08-24 14:59:03 +0700

    change selector variablr

--- Exercise/form-comment/index.js
+++ Exercise/form-comment/index.js
@@ -1,12 +1,17 @@
 var comments = [];
-var $formComment = document.querySelector(".comment-form");
-$formComment.addEventListener("submit", function (event) {
+var $formComment = document.querySelector('.comment-form');
+$formComment.addEventListener('submit', function (event) {
   event.preventDefault();
 
-  var name = document.querySelector("#name").value;
-  var content = document.querySelector("#content").value;
+
+  var $name = document.querySelector('#name'); 
+  var $content = document.querySelector('#content');
+  
+  var name = $name.value;
+  var content = $content.value;
   var date;
-  currDate = new Date();
+  var currDate = new Date();
+  
   date = currDate.getDate() + ' / '+ (currDate.getMonth()+1) + ' / ' + currDate.getFullYear();
   var comment = {
     name,
@@ -20,8 +25,8 @@ $formComment.addEventListener("submit", function (event) {
 });
 
 function updateComments() {
-  var $comments = document.querySelector(".comments");
-  $comments.innerHTML = "";
+  var $comments = document.querySelector('.comments');
+  $comments.innerHTML = '';
 
   for (var comment of comments) {
     var item = `
